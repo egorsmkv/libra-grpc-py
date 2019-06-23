5afe7121ccfd3d14edad64a031e278d75e62813e
Yehor Smoliakov <yehor@smoliakov>
2019-06-23 13:06:07 +0300

    Fixed converting address to bytes

--- examples/account_state.py
+++ examples/account_state.py
@@ -3,7 +3,6 @@ import grpc
 from lib.admission_control_pb2_grpc import AdmissionControlStub
 from lib.get_with_proof_pb2 import UpdateToLatestLedgerRequest, GetAccountStateRequest, RequestItem
 from examples.config import SERVER_ADDRESS
-from examples.util import reverse_bytes
 
 ADDRESS = '4bb23eb092a04fae7dea56fffb88322f993017e9bb8e9822cb441c80862d78e6'
 
@@ -12,12 +11,13 @@ def run():
     channel = grpc.insecure_channel(SERVER_ADDRESS)
     stub = AdmissionControlStub(channel)
 
-    account = GetAccountStateRequest(address=reverse_bytes(ADDRESS))
+    account = GetAccountStateRequest(address=bytes.fromhex(ADDRESS))
     item = RequestItem(get_account_state_request=account)
     request = UpdateToLatestLedgerRequest(client_known_version=125190, requested_items=[item])
     response = stub.UpdateToLatestLedger(request)
 
-    print(response)
+    state = response.response_items[0].get_account_state_response.account_state_with_proof
+    print(state)
 
 
 if __name__ == '__main__':
--- examples/util.py
+++
@@ -1,5 +0,0 @@
-def reverse_bytes(value):
-    value = bytearray.fromhex(value)
-    value.reverse()
-
-    return bytes(value)
